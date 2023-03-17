3e9bfe13009e4ebbb105168e5cb9e7c85bb81c7d
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2023-03-16 19:32:21 -0700

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+	public class MoviesController : Controller
+	{
+		public IActionResult Random()
+		{
+			var movie = new Movie()
+			{
+				Name = "Shrek!"
+			};
+
+			return View(movie);
+		}
+
+		public IActionResult NotRandom()
+		{
+			var movie = new Movie()
+			{
+				Name = "Not Shrek!"
+			};
+
+			return View(movie);
+		}
+	}
+}
---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace Vidly.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+	}
+
+	// /movies/random
+}
