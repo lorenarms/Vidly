501ce2f25b20e59b15b2150658049755d49c34ac
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2023-04-16 19:52:03 -0700

    update with users

--- UsersAndRolesTutorial/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace UsersAndRolesTutorial.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
